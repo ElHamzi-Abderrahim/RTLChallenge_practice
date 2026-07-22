a54635f115a3467bc4986354601c856d395d4645
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-22 01:18:44 -0400

    Solving #9 Parameterizable Ring counter

--- questions/0003/interface.sv
+++ questions/0003/interface.sv
@@ -5,6 +5,23 @@ module ring_counter #(
     input  wire                                rst_n,
     output wire [COUNTER_WIDTH-1:0]            count_out
 );
-// your implementation here
+
+    logic [COUNTER_WIDTH-1:0] count_out_reg ;
+
+    always_ff @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            count_out_reg <= 'b1 ;
+        end
+        else begin
+            for(integer i=1; i < COUNTER_WIDTH; i = i+1) begin
+                count_out_reg[i] <= count_out_reg[i-1] ;
+            end
+
+            count_out_reg[0] <= count_out_reg[COUNTER_WIDTH-1] ;
+        end
+    end
+
+    assign count_out = count_out_reg ;
+
 
 endmodule
