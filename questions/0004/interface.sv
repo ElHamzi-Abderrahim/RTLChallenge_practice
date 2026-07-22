e00976c40a4e04d27485c5ad5318ebe56ff0c04b
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-22 01:27:36 -0400

    Solving #11 Parameterizable Ripple Counter

--- questions/0004/interface.sv
+++ questions/0004/interface.sv
@@ -5,6 +5,17 @@ module ripple_counter #(
     input  wire                                rst_n,
     output wire [COUNTER_WIDTH-1:0]            count_out
 );
-// your implementation here
+
+    reg [COUNTER_WIDTH-1:0] count_out_reg ;
+
+    always_ff @( posedge clk, negedge rst_n ) begin
+        if(!rst_n) begin
+            count_out_reg <= {COUNTER_WIDTH{1'b0}} ;
+        end else begin
+            count_out_reg <= (count_out_reg == {COUNTER_WIDTH{1'b1}}) ? {COUNTER_WIDTH{1'b0}} : count_out_reg+1 ;
+        end
+    end
+
+    assign count_out = count_out_reg ;
 
 endmodule
