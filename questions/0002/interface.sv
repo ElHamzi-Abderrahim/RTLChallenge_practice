7fe670594f51a68621650d6351c41e19458343d7
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-22 00:52:26 -0400

    Solving #6 Parameterizable Binary Multiplier using built-in operator '*'

--- questions/0002/interface.sv
+++ questions/0002/interface.sv
@@ -5,6 +5,7 @@ module multiplier #(
     input  wire [INPUT_WIDTH-1:0]              data_in_2,
     output wire [2*INPUT_WIDTH-1:0]                data_out
 );
-// your implementation here
+
+    assign data_out = data_in_1 * data_in_2 ;
 
 endmodule
