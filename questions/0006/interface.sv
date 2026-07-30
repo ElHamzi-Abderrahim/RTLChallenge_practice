c9475108fcbf96dd61900f161f34756b47fd9d92
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-30 07:02:40 -0400

    Solving #17 Parameterizable dual edge flip-flop

--- questions/0006/interface.sv
+++ questions/0006/interface.sv
@@ -6,6 +6,27 @@ module dual_edge_dff #(
     input  wire [DATA_WIDTH-1:0]               data_in,
     output wire [DATA_WIDTH-1:0]               data_out
 );
-// your implementation here
+
+    reg [DATA_WIDTH-1:0] data_out_p, data_out_n, data_out_ff ;
+
+    assign data_out_ff  = (clk) ? data_out_p : data_out_n ; // implement a mux
+    
+    assign data_out     = (rst_n) ? data_out_ff : {DATA_WIDTH{1'b0}} ; // mux for asynchronous reset
+
+    always_ff @( posedge clk, negedge rst_n ) begin : pos_edge_ff
+        if (!rst_n) begin
+            data_out_p <= {DATA_WIDTH{1'b0}};
+        end else begin
+            data_out_p <= data_in ;
+        end
+    end
+    
+    always_ff @( negedge clk, negedge rst_n ) begin : neg_edge_ff
+        if (!rst_n) begin
+            data_out_n <= {DATA_WIDTH{1'b0}};
+        end else begin
+            data_out_n <= data_in ;
+        end
+    end
 
 endmodule
