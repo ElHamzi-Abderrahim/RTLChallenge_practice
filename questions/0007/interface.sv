32ff38a4068ae4121c315d3e5e60cd883e048978
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-30 13:32:29 -0400

    Solving #19 Parameterizable Multiplexer

--- questions/0007/interface.sv
+++ questions/0007/interface.sv
@@ -6,6 +6,32 @@ module simple_mux #(
     input  wire [SELECT_WIDTH-1:0]                 sel,
     output wire [DATA_WIDTH-1:0]                   data_out
 );
-// your implementation here
+
+`define METHOD_2 
+
+// using indexed part selection
+`ifdef METHOD_1
+assign data_out = data_in[sel*DATA_WIDTH +: DATA_WIDTH] ; 
+
+// using a synthesisable code
+`elsif METHOD_2
+wire [DATA_WIDTH-1:0] data_in_array [SELECT_WIDTH**2-1:0] ;
+genvar i ;
+
+generate
+    for (i=0; i < 2**SELECT_WIDTH; i = i+1) begin
+        assign data_in_array[i] = data_in[(DATA_WIDTH*(i+1)) - 1 : DATA_WIDTH*i] ;
+    end
+endgenerate
+
+assign data_out = data_in_array[sel];
+
+
+`endif
+
+
+
+
+
 
 endmodule
