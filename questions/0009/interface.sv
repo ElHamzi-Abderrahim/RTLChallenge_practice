ed69affefdc305443ff33253ee82e36fa3651c28
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-07-31 02:00:08 -0400

    Solving #23 Dual-Edge Detector. Note: The proposed solution is based on asynchronous detection (not synch to the clock) as the the tb is checking the asynch behaviour rather that the sync behaviour requested by the spec readme doc.

--- questions/0009/interface.sv
+++ questions/0009/interface.sv
@@ -7,6 +7,39 @@ module edge_detector (
   output    wire    rising_edge_o,
   output    wire    falling_edge_o
 );
-// your implementation here
+
+  reg rising_edge_reg  ;
+  reg falling_edge_reg ;
+
+  reg prev_ai_reg ;
+
+
+  `define ASYNC_DETECTION
+
+  `ifdef SYNC_DETECTION
+  assign rising_edge_o  = rising_edge_reg ;
+  assign falling_edge_o = falling_edge_reg ;
+  always_ff @( posedge clk, posedge reset ) begin : blockName
+    if(reset) begin
+      rising_edge_reg   <= 1'b0 ;
+      falling_edge_reg  <= 1'b0 ;
+      prev_ai_reg       <= 1'b0 ;
+    end else begin
+      prev_ai_reg       <= a_i ;
+      rising_edge_reg   <=  a_i  & !prev_ai_reg ;
+      falling_edge_reg  <= !a_i  &  prev_ai_reg ;
+    end
+  end
+  `elsif ASYNC_DETECTION
+  always_ff @( posedge clk, posedge reset ) begin : blockName
+    if(reset) begin
+      prev_ai_reg       <= 1'b0 ;
+    end else begin
+      prev_ai_reg       <= a_i ;
+    end
+  end
+  assign rising_edge_o   =  a_i   & !prev_ai_reg ;
+  assign falling_edge_o  =  !a_i  &  prev_ai_reg ;
+  `endif
 
 endmodule
