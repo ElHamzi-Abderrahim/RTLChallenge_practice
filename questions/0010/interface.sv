9af2b5342c27ea5522a55bbf300231a400c9842f
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2026-08-01 03:33:40 -0400

    Solving #25 Combinational ALU (ADD, SUB, AND, OR, XOR, NOT, SLL, SRL)

--- questions/0010/interface.sv
+++ questions/0010/interface.sv
@@ -6,6 +6,37 @@ module simple_alu (
   output    reg [7:0]    alu_o
 );
 
-// your implementation here
+  parameter ADD_OPCODE = 3'b000 , 
+            SUB_OPCODE = 3'b001 ,
+            AND_OPCODE = 3'b010 ,
+            OR_OPCODE  = 3'b011 ,
+            XOR_OPCODE = 3'b100 ,
+            NOT_OPCODE = 3'b101 ,
+            SLL_OPCODE = 3'b110 ,
+            SRL_OPCODE = 3'b111 
+          ;
+
+  always_comb begin : alu_comb
+    case (op_i)
+      ADD_OPCODE:
+        alu_o = a_i + b_i ;
+      SUB_OPCODE:
+        alu_o = a_i - b_i ;
+      AND_OPCODE:
+        alu_o = a_i & b_i ;
+      OR_OPCODE:
+        alu_o = a_i | b_i ;
+      XOR_OPCODE:
+        alu_o = a_i ^ b_i ;
+      NOT_OPCODE:
+        alu_o = ~a_i ;
+      SLL_OPCODE:
+        alu_o = a_i << 1 ;
+      SRL_OPCODE:
+        alu_o = a_i >> 1 ;
+      default: 
+        alu_o = {8{1'b0}} ;
+    endcase
+  end
 
 endmodule
